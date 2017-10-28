9a764099407c19737167a57caf31fa06e2e38304
Brandio <strochba@mail.uc.edu>
2017-10-28 18:25:25 -0400

    Added the ply reading script

---
+++ Scripts/PlyImporter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+[RequireComponent(typeof(PointDisplayer))]
+// Reads .ply files and creates a list of points
+// Will only work with ply files formatted the way the python photogrammetry toolbox creates
+public class PlyImporter : MonoBehaviour {
+
+	// File path for ply file to import
+	public string plyFilePath = "/home/branden/Desktop/models0/pmvs_options.txt.ply";
+
+	PointDisplayer pointDisplayer;
+
+	public void readPly(string path)
+	{
+		StreamReader reader = new StreamReader(plyFilePath);
+
+		// Make sure file is formatted corectly
+		string line = reader.ReadLine();
+		if(line != "ply")
+		{
+			Debug.LogError("This is not a correctly formatted ply file!!!");
+			return;
+		}
+		
+		bool header = true;
+		List<PointDisplayer.Point> points = new List<PointDisplayer.Point>();
+		while(line != null)
+		{
+			if(header)
+			{
+				if(line == "end_header")
+				{
+					header = false;
+				}
+			}
+			else
+			{
+				string[] stringArray = line.Split(null);
+				if(stringArray.Length != 9)
+					continue;
+				PointDisplayer.Point point = new PointDisplayer.Point(float.Parse(stringArray[0]),float.Parse(stringArray[1]),float.Parse(stringArray[2]),
+										      float.Parse(stringArray[3]),float.Parse(stringArray[4]),float.Parse(stringArray[5]),
+										      float.Parse(stringArray[6]),float.Parse(stringArray[7]),float.Parse(stringArray[8]));
+				points.Add(point);
+
+			}
+			line = reader.ReadLine();
+			
+		}
+		pointDisplayer.setPointCloud(points);
+		reader.Close();
+	}
+
+	void Start () {
+		pointDisplayer = this.GetComponent(typeof(PointDisplayer)) as PointDisplayer;
+		readPly(plyFilePath);
+	}
+}
---
+++ Scripts/PointDisplayer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointDisplayer : MonoBehaviour {
+
+	public struct Point
+	{	
+		public Point(float xx,float yy,float zz,float nxx,float nyy,float nzz,float rr, float gg, float bb)
+		{
+			x = xx;
+			y = yy;
+			z = zz;
+			nX = nxx;
+			nY = nyy;
+			nZ = nzz;
+			r = rr;
+			g = gg;
+			b = bb;
+		}
+		public float x;
+		public float y;
+		public float z;
+
+		public float nX;
+		public float nY;
+		public float nZ;
+
+		public float r;
+		public float g;
+		public float b;
+	}
+
+	const int MAX_POINTS_IN_MESH = 65000;
+	public void setPointCloud(List<Point> points)
+	{
+		int numMeshes = points.Count/MAX_POINTS_IN_MESH;
+		// I dont think this will ever be the case so just log and return for now
+		if(numMeshes > 1)
+		{
+			Debug.LogError("Too many points for one mesh!!!");
+			return;
+		}
+
+		GameObject meshObject = new GameObject();
+		meshObject.transform.parent = this.transform;
+		MeshFilter filter = meshObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+		MeshRenderer renderer = meshObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		Mesh mesh = filter.mesh;
+
+		Vector3[] verts = new Vector3[points.Count];
+		Vector3[] normals = new Vector3[points.Count];
+		Color[] colors = new Color[points.Count];
+		int[] indices = new int[points.Count];
+		for(int i = 0; i < points.Count; i++)
+		{	
+			verts[i] = new Vector3(points[i].x,points[i].y,points[i].z);
+			normals[i] = new Vector3(points[i].nX,points[i].nY,points[i].nZ);
+			colors[i] = new Color(points[i].r/100,points[i].g/100,points[i].b/100);
+
+			indices[i] = i;
+		}
+		mesh.vertices = verts;
+		mesh.colors = colors;
+		mesh.normals = normals;
+		mesh.SetIndices(indices,MeshTopology.Points,0);
+
+		Material mat = new Material(Shader.Find("Particles/Multiply"));
+		renderer.material = mat;	
+	}
+}
