5a202128f418462d681eb111809d1bc14411ff83
Brandio <strochba@mail.uc.edu>
2017-11-02 12:14:52 -0400

    Modify octree a bit more

--- Scripts/PoissonReconstruction.cs
+++ Scripts/PoissonReconstruction.cs
@@ -5,8 +5,11 @@ public class PoissonReconstruction : MonoBehaviour {
 
     PointOctree<PlyImporter.Point> octree = null;
 	// The higher the octree depth the more detail of the surface
-	public void reconstruct (List<PlyImporter.Point> points, int maxOctreeDepth = 6) {
-        octree = new PointOctree<PlyImporter.Point>(2, new Vector3(0, 0, 0), 1, 12);
+	public void reconstruct (List<PlyImporter.Point> points, float maxOctreeDepth = 6) {
+        float maxSize = .0001f;
+        float minOctreeSize = Mathf.Sqrt(3) * Mathf.Sqrt(maxSize * maxSize * Mathf.Pow(2.71828f,(-2 * maxOctreeDepth)));
+        Debug.Log(minOctreeSize);
+        octree = new PointOctree<PlyImporter.Point>(maxSize, new Vector3(0, 0, 0), minOctreeSize, (int)maxOctreeDepth);
         foreach (PlyImporter.Point point in points)
         {
             octree.Add(point, new Vector3(point.x, point.y, point.z));
