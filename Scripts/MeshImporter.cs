04c17c18288c89238fc1edfc0021bffc8e6ab64b
Brandio <strochba@mail.uc.edu>
2017-11-10 15:52:53 -0500

    got that mesh importing up boi

--- Scripts/MeshImporter.cs
+++ Scripts/MeshImporter.cs
@@ -8,17 +8,25 @@ using System.IO;
 public class MeshImporter : MonoBehaviour {
 
     // File path for ply file to import
-    public string plyFilePath = "/home/branden/Desktop/models0/pmvs_options.txt.ply";
+    public string plyFilePath = "C:/Users/Branden/Documents/Point Cloud Surface Reconstruction/Assets/Unity-PointCloud/mesh.ply";
 
-    public Vector3[] newVertices;
-    public int[] newTriangles;
+    Vector3[] newVertices;
+    int[] newTriangles;
 
     void CreateMesh()
     {
+        GameObject meshObject = new GameObject();
+        meshObject.transform.parent = this.transform;
+        MeshFilter filter = meshObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        MeshRenderer renderer = meshObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+        Material material = new Material(Shader.Find("Standard"));
+
         Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        filter.mesh = mesh;
         mesh.vertices = newVertices;
         mesh.triangles = newTriangles;
+
+        renderer.material = material;
     }
 
     void LoadPly()
@@ -26,33 +34,65 @@ public class MeshImporter : MonoBehaviour {
         StreamReader reader = new StreamReader(plyFilePath);
         string line = reader.ReadLine();
         bool header = true;
+        int numberVerts = 0;
+        int numberTriangles = 0;
+        int postHeaderIndex = 0;
+        // Parse file to fill newTriangles and newVertices
         while (line != null)
         {
+            
             if (header)
             {
-                if (line == "end_header")
+                if(line.Contains("element vertex"))
+                {
+                    string[] stringArray = line.Split(null);
+                    numberVerts = int.Parse(stringArray[2]);
+                    newVertices = new Vector3[numberVerts];
+                }
+                else if(line.Contains("element face"))
+                {
+                    string[] stringArray = line.Split(null);
+                    numberTriangles = int.Parse(stringArray[2]) * 3;
+                    newTriangles = new int[numberTriangles];
+                }
+                else if (line == "end_header")
                 {
                     header = false;
                 }
             }
             else
             {
-                string[] stringArray = line.Split(null);
-                if (stringArray.Length != 6)
-                    continue;
-                //Point point = new Point(float.Parse(stringArray[0]), float.Parse(stringArray[1]), float.Parse(stringArray[2]),
-                                              float.Parse(stringArray[3]), float.Parse(stringArray[4]), float.Parse(stringArray[5]),
-                                              float.Parse(stringArray[6]), float.Parse(stringArray[7]), float.Parse(stringArray[8]));
-                //points.Add(point);
+                // Vertex first
+                if(postHeaderIndex < numberVerts)
+                {
+                    string[] stringArray = line.Split(null);
+
+                    Vector3 vertex = new Vector3(float.Parse(stringArray[0]), float.Parse(stringArray[1]), float.Parse(stringArray[2]));
+                    newVertices[postHeaderIndex] = vertex;
+                }
+                else
+                {
+                    string[] stringArray = line.Split(null);
+
+                    newTriangles[(postHeaderIndex - numberVerts) * 3] = int.Parse(stringArray[1]);
+                    newTriangles[(postHeaderIndex - numberVerts) * 3 + 1] = int.Parse(stringArray[3]);
+                    newTriangles[(postHeaderIndex - numberVerts) * 3 + 2] = int.Parse(stringArray[5]);
+                }
+                postHeaderIndex++;
+                if (postHeaderIndex > numberVerts + numberTriangles)
+                {
+                    break;
+                }
             }
             line = reader.ReadLine();
-
         }
+        Debug.Log(numberVerts + " " + numberTriangles);
     }
 	// Use this for initialization
 	void Start () {
-	
-	}
+        LoadPly();
+        CreateMesh();
+    }
 	
 	// Update is called once per frame
 	void Update () {
